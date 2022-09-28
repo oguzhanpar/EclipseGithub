47bf66645eb795aafb8f804628acb981043aebd2
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-28 14:26:31 +0300

    dasdasds

--- javaDemos/src/javaHelloWorld/Main.java
+++ javaDemos/src/javaHelloWorld/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 		System.out.println("Hello World !");
 		System.out.println("Hello World 2 !");
-		System.out.println("Hello World 2 !");
+		System.out.println("Hello World 3 !");
 
 
 		
